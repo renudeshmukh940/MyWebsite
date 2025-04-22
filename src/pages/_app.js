c28bbdcb5edb29999b8aa437805903541117fde3
renudeshmukh940 <deshmukhrenu4@gmail.com>
2025-04-22 23:26:08 +0530

    Initial Next.js site commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,4 @@
-import Footer from '@/components/Footer';
+import Footer from '../components/Footer';
 import NavBar from '@/components/NavBar'
 import '@/styles/globals.css'
 import { Montserrat } from "next/font/google";
